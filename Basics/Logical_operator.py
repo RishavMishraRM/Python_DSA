110553d6ac84626afa52f6ac417b047ca716186c
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-24 09:57:43 +0530

    logic & realtional push

---
+++ Basics/Logical_operator.py
@@ -0,0 +1,23 @@
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
+print(not y)
+
+a = -10
+b = False
+c = ''
+print(not a)
+print(not c)
+
+x = 10
+y = 20
+print(x and y)
+
+
+x = 0
+y = 20
+print(x and y)
+print(x or y)
---
+++ Basics/Relational&Comparison.py
@@ -0,0 +1,46 @@
+x = 10
+y = 20
+
+print(x < y) 
+print(x > y)
+print(x >= y)
+
+
+x = -10
+y = 20
+
+print(x < y) 
+
+
+x = 10
+y = -20
+
+print(x > y) 
+
+
+x = "Hello"
+y = "Python"
+print(x > y)
+print(x < y)
+
+cx = "H"
+cy = "P"
+ord(cx)
+ord(cy)
+print(ord(cx))
+print(ord(cy))
+
+
+x = 10
+y = 10.0
+print(x == y)
+print(x != y)
+
+print("Hello" == "Hello")
+print("Hello" == "Python")
+print("Hello" == "hello")
+
+
+print("cx" == 72)
+
+print(10 < 20 != 30 < 40)
