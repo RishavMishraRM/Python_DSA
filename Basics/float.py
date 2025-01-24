add06fdb608538135c6b89e37bc90444fb864040
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-24 09:33:43 +0530

    basics push

---
+++ Basics/float.py
@@ -0,0 +1,21 @@
+no = 5 + 5
+print(type(no))
+
+a = 4.2 + 5
+print(type(a))
+
+b = 10/5
+print(type(b))
+
+c = 14%4
+print(c)
+print(type(c))
+
+c = 14.75%4
+print(c)
+print(type(c))
+
+d = 3.5 ** 2
+print(d)
+print(type(d))
+
---
+++ Basics/floor&ceil.py
@@ -0,0 +1,12 @@
+a = 15//4
+print(a)
+print(type(a))
+
+b = 15.5//4
+print(b)
+print(type(b))
+
+c = -5//4
+print(c)
+print(type(c))
+
