fac6e8d75ac2b96191a80c665c598da17841c50d
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-28 07:30:01 +0530

    basics push

---
+++ Basics/input.py
@@ -0,0 +1,26 @@
+x, y = 10, 20
+
+print(x+y)
+
+x = input()
+print(x)
+
+x = input('Enter a value: ')
+print(x)
+print(type(x))
+
+x = int(input('Enter a value of X: '))
+y = int(input('Enter a value of Y: '))
+print(x)
+print(type(x))
+
+size = int(input('Enter a value: '))
+values = [int(input('Enter a value: ')) for _ in range(size)]
+
+print(values)
+
+x = input('Enter value of X: ')
+y = input('Enter value of Y: ')
+
+print(eval(x) + eval(y))
+
