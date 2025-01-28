37d0b3720d7e285242a3d9ac4cf1c3e0d49461c0
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-28 09:08:32 +0530

    list added

---
+++ Fundamental/List.py
@@ -0,0 +1,33 @@
+l = [10, 20, 25, 35, 85]
+
+print(l)
+print(l[1])
+print(l[-1])
+print("Type of List: ", type(l))
+print("Length of List : ", len(l))
+
+l = [10, 'Hello', 2.85]
+
+print(l)
+print(l[1])
+print(type(l[1]))
+print(l[2])
+print(type(l[2]))
+print(l[3])
+print(type(l[3]))
+print("Type of List: ", type(l))
+print("Length of List : ", len(l))
+
+
+l = [10, 'Hello', 2.85]
+
+print(l)
+l[1] = 'World'
+print(l)
+
+
+
+l = [10, 'Hello', 2.85]
+l2 = l + [5.26, 'Python']
+print(l2)
+
