48567796d6d29ebb21488af4958ccc8b06831498
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-28 09:22:40 +0530

    list_2 added

---
+++ Fundamental/List_2(membership_operator).py
@@ -0,0 +1,24 @@
+l1 = [10, 20, 30, 40]
+
+print(99 in l1)
+
+print(99 not in l1)
+
+
+l1 = [10, 20, 30, 40]
+l2 = [10, 20, 30, 40]
+
+
+print(l1 is l2)
+print(l1 == l2)
+
+
+l1 = [10, 20, 30, 40]
+l2 = [40, 20, 30, 10]
+
+print(l1 is l2)
+print(l1 == l2)
+
+
+l2 = l1
+print(l1 is l2)
---
+++ Fundamental/Tuples.py
@@ -0,0 +1,23 @@
+t = (10, 20, 30, 40)
+
+print(t)
+print(type(t))
+print(len(t))
+print(t[0])
+
+
+
+t = (10, "Hello", 2.85)
+print(t)
+
+print(t[0])
+print(type(t[0]))
+print(t[1])
+print(type(t[1]))
+print(t[2])
+print(type(t[2]))
+print(t[-2])
+print(type(t))
+print(len(t))
+
+
