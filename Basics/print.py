1972c7b8bf92735a4856aa7a652b962330840f99
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-28 07:38:01 +0530

    Push

---
+++ Basics/print.py
@@ -0,0 +1,22 @@
+print('Hello')
+
+x = 10
+print()
+print(x)
+
+print('Hello' + 'World')
+
+print('Hello'* 9)
+
+print('Hello'* 9 + 'World')
+
+x = 10
+y = 20
+z = 30
+print(x, y, z)
+
+x = '10'
+y = '20'
+z = '30'
+print(x+y+z)
+
