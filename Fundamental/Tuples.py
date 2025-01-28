12334a958a1cbf398380d243d1ee5cc0b33ede70
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-28 09:23:01 +0530

    tuples added

--- Fundamental/Tuples.py
+++ Fundamental/Tuples.py
@@ -21,3 +21,12 @@ print(type(t))
 print(len(t))
 
 
+t = (10, "Hello", 2.85)
+t1 = t + (56.5, 'Python', 99)
+print(t1)
+
+
+# t[1]= 20 # TypeError: 'tuple' object does not support item assignment
+del t1  # Delete the tuple
+#del t[0] # TypeError: 'tuple' object doesn't support item deletion 
+
