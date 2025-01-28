a36a9870f748132b872774553fc92a4a95416b24
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-28 09:52:31 +0530

    function

---
+++ Fundamental/function.py
@@ -0,0 +1,33 @@
+def display():
+    print("Hello")
+
+display()
+display()
+display()
+
+
+def display():
+    print("Hello")
+    print("Python")
+
+
+def mysum(a, b):
+    c = a+b
+    return c
+
+
+display()
+x = mysum(10, 20)
+print(x)
+
+def mymul(a, b):
+    c = a*b
+    return c
+
+y = mymul(10, 20)
+print(y)
+
+def display(name):
+    print("Hello", name)
+
+display("Rishav")
