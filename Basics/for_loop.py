5f11a8af3783585e21582583d28f2a07a620279f
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-28 08:25:00 +0530

    for loop

---
+++ Basics/for_loop.py
@@ -0,0 +1,53 @@
+for ch in 'hello':
+    print(ch)
+
+
+for ch in 'hello':
+    print(ch, end=' ')
+
+
+x = range(5)
+print(x)
+
+for i in x:
+    print(i)
+
+
+
+
+x = range(3, 7)
+print(x)
+
+for i in x:
+    print(i)
+
+
+
+# Even numbers
+x = range(0, 10, 2)
+print(x)
+
+for i in x:
+    print(i)
+
+
+
+# Odd numbers
+x = range(1, 10, 2)
+print(x)
+
+for i in x:
+    print(i)
+
+
+
+# Negative numbers
+x = range(10, 1, -2)
+print(x)
+
+for i in x:
+    print(i) 
+
+
+
+
