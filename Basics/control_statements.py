f71be0a22cfa6068ee6c18150c5a785b5658f6be
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-28 07:54:25 +0530

    control statement

---
+++ Basics/control_statements.py
@@ -0,0 +1,33 @@
+n = int(input("Enter a value: "))
+
+print('Checking the number n')
+
+if n >= 0:
+    print('n is positive')
+    print('Value of n is:', n)
+
+print('End of the program')
+
+
+
+
+
+
+n = int(input("Enter a value: "))
+
+print('Checking the number n')
+
+if n >= 0:
+    print('positive number')
+    print('Value of n is:', n)
+else:
+    print('negative number')
+    print('Value of n is:', n)
+
+
+print('End of the program')
+
+
+
+
+
---
+++ Basics/nested_control_statements.py
@@ -0,0 +1,9 @@
+n = int(input("Enter Value: "))
+
+if n >= 0:
+    if n == 0:
+        print("Zero Number")
+    else:
+        print("Positive Number")
+else:
+    print("Negative Number")
