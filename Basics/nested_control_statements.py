f352ad6eca4a7116f8ffed9dd2e00cbf4aa38ed6
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-28 07:59:47 +0530

    nested control flow

--- Basics/nested_control_statements.py
+++ Basics/nested_control_statements.py
@@ -7,3 +7,31 @@ if n >= 0:
         print("Positive Number")
 else:
     print("Negative Number")
+
+
+
+
+
+n = int(input("Enter Value: "))
+
+if n > 0:
+    print("Positive Number")
+elif n == 0:
+    print("Zero Number")
+else:
+    print("Negative Number")
+
+
+
+n = int(input("Enter Value: "))
+
+if n > 0:
+    print("Positive Number")
+elif n < 0:
+    print("Zero Number")
+else:
+    print("Negative Number")
+
+
+
+
