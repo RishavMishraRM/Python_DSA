9c675afd550042c515b9464983681b0642bba8c0
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-29 08:17:53 +0530

    import module

---
+++ Fundamental/Import_Modules.py
@@ -0,0 +1,40 @@
+import math
+
+print(math.pi)
+print(math.e)
+print(math.sqrt(16))
+print(math.pow(2,3))
+
+
+
+import math as m
+
+print(m.pi)
+print(m.e)
+print(m.sqrt(16))
+print(m.pow(2,3))
+
+
+from math import pi, e, sqrt, pow
+
+print(pi)
+print(e)
+print(sqrt(16))
+print(pow(2,3))
+
+
+from math import *
+
+print(pi)
+print(e)
+print(sqrt(16))
+print(pow(2,3))
+
+
+from math import pi as p, e as E, sqrt as s, pow as P
+
+print(p)
+print(E)
+print(s(16))
+print(P(2,3))
+
