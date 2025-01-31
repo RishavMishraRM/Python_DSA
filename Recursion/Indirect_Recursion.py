d6a7890722d0620fdda4b949ef9e48d495be9958
RishavMishraRM <rishav.mishra.rkm@gmail.com>
2025-01-31 08:05:56 +0530

    Indirect recursion added

---
+++ Recursion/Indirect_Recursion.py
@@ -0,0 +1,11 @@
+def calculateA(n):
+    if n > 0:
+        '''1st over'''
+        calculateB(n-1)
+        ''''''
+
+def calculateB(n):
+    if n > 0:
+        '''2nd over'''
+        calculateA(n-1)
+        ''''''
